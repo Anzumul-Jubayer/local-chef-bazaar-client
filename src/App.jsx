c8248151d5a1a193bbac38dfc245569220125ff8
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-12-06 23:08:51 +0600

    client-setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl'>Welcome</h1>
+      <button className="btn">Default</button>
+    </>
+  )
+}
+
+export default App
