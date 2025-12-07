f48fd99566bcd438ff649811b4405a1865a50aa6
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-12-07 22:36:30 +0600

    login-page

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { createBrowserRouter } from "react-router";
-
 import Home from "../Pages/Home/Home";
 import MainLayout from "../Layouts/Mainlayout";
+import Login from "../Pages/Auth/Login";
+import Register from "../Pages/Auth/Register";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,14 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path:'/login',
+        element: <Login/> ,
+      },
+      {
+        path:'/register',
+        element: <Register/> ,
       },
     ],
   },
