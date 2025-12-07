3f17f1480409276aa402290a32af9825088a6f2d
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-12-07 23:09:07 +0600

    dynamic-route

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import Banner from '../../Components/Home/Banner';
-import ExtraSection from '../../Components/Home/ExtraSection';
+import React from "react";
+import Banner from "../../Components/Home/Banner";
+import ExtraSection from "../../Components/Home/ExtraSection";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
-    return (
-        <div>
-          <Banner/>
-          <ExtraSection/>
-        </div>
-    );
+  return (
+    <div>
+      <Helmet>
+        <title>Home | LocalChefBazaar</title>
+      </Helmet>
+      <Banner />
+      <ExtraSection />
+    </div>
+  );
 };
 
 export default Home;
